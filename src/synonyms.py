cf0e4a39da7775b6158acf413e268280f5af1b72
Zabaznova Anzhelika <zabaznova2303@gmail.com>
2022-10-02 13:50:19 +0300

    Реализован интерактивный режим поиска синонимов с командной строки

--- src/synonyms.py
+++ src/synonyms.py
@@ -1,24 +1,53 @@
 import argparse
+import py4j.protocol
 from modules import word2vec
 import modules.common as common
 
-def main():
-    model = word2vec.get_word2vec_model()
+model = None
 
-    persons = ["Андрей Бочаров", "Владимир Марченко", "Бочаров", "Вячеслав Черепахин", "Виталий Лихачев",
-               "Валерий Бахин",  "Анатолий Себелев", "Денис Долгов ", "Николай Алимов", "Ольга Зубарева"]
+def get_model():
+    global model
+    if model is None:
+        model = word2vec.get_word2vec_model()
+    return model
 
-    for p in persons:
-        p = common.clear_lemma(p)
-        print(f'Синонимы для {p}')
-        model.findSynonyms(p, 5).show()
-        print('----')
+# Определяет синонимы для слова
+def get_synonyms(word, count=5):
+    model = get_model()
+    word = common.clear_lemma(word)
+    try:
+        return model.findSynonyms(word, count)
+    except py4j.protocol.Py4JJavaError:
+        return None
+
+
+def interactive_mode():
+    while True:
+            try:
+                word = input('Input word: ')
+                synonyms = get_synonyms(word)
+                if synonyms:
+                    print('Synonyms:')
+                    synonyms.show()
+                else:
+                    print('No synonyms')
+            except KeyboardInterrupt:
+                break
+
+def main(cfg):
+    if cfg.interactive:
+        interactive_mode()
+        return
+
+    print('Nothing to do')
 
 # Парсинг аргументов командной строки
 def parse_args():
     parser = argparse.ArgumentParser(description='Find synonyms using word2vec')
+    parser.add_argument('-i', '--interactive', action=argparse.BooleanOptionalAction,
+        help='Interactive mode')
     return parser.parse_args()
 
 if __name__ == '__main__':
     cfg = parse_args()
-    main()
+    main(cfg)
