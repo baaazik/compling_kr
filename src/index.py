70ff5f46e81b34abb1810fcea7ab8652078cdb5f
Zabaznova Anzhelika <zabaznova2303@gmail.com>
2022-10-24 21:41:14 +0300

    Добавлена страница со списком новостей

--- src/index.py
+++ src/index.py
@@ -1,20 +1,48 @@
 from flask import Flask
-import flask
+from flask import render_template, request
+import pymongo
+import math
+
+from modules import common
+
+ITEMS_PER_PAGE = 100
 
 app = Flask(__name__)
+
+# Определяет номер страницы на основе аргументов
+def get_page_index():
+    try:
+        return int(request.args.get('page', 0))
+    except ValueError:
+        return 0
+
+# Дополняет запрос к базе пагинацией (берет записи только для нужной странице)
+def paginate(data):
+    page = get_page_index()
+    data = data.skip(page * ITEMS_PER_PAGE).limit(ITEMS_PER_PAGE)
+    total = data.count()
+    pages = math.ceil(total / float(ITEMS_PER_PAGE))
+    return data, pages
 
 @app.route('/')
 @app.route('/news')
 def news():
-    return flask.render_template('news.html', title='Новости')
+    db = common.get_db()
+    news_cl = db['news']
+    page = get_page_index()
+
+    news = news_cl.find({}).sort([('date', pymongo.DESCENDING)])
+    news, pages = paginate(news)
+
+    return render_template('news.html', title='Новости', news=news, pages=pages, curpage=page)
 
 @app.route('/facts')
 def facts():
-    return flask.render_template('facts.html', title='Факты')
+    return render_template('facts.html', title='Факты')
 
 @app.route('/sentiment')
 def sentiment():
-    return flask.render_template('sentiment.html', title='Тональность')
+    return render_template('sentiment.html', title='Тональность')
 
 if __name__ == '__main__':
     app.run(debug=True)
