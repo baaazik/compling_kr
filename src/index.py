5d8e4b47e8fad7aea224756ad693f67f86e26141
Zabaznova Anzhelika <zabaznova2303@gmail.com>
2022-10-25 23:15:41 +0300

    Добавление таблицы со списком фактов

--- src/index.py
+++ src/index.py
@@ -8,11 +8,15 @@ from modules import common
 ITEMS_PER_PAGE = 100
 
 app = Flask(__name__)
+db = common.get_db()
 
 # Определяет номер страницы на основе аргументов
 def get_page_index():
     try:
-        return int(request.args.get('page', 0))
+        page = int(request.args.get('page', 0))
+        if page < 0:
+            page = 0
+        return page
     except ValueError:
         return 0
 
@@ -24,10 +28,10 @@ def paginate(data):
     pages = math.ceil(total / float(ITEMS_PER_PAGE))
     return data, pages
 
+# Страница новостей
 @app.route('/')
 @app.route('/news')
 def news():
-    db = common.get_db()
     news_cl = db['news']
     page = get_page_index()
 
@@ -36,10 +40,17 @@ def news():
 
     return render_template('news.html', title='Новости', news=news, pages=pages, curpage=page)
 
+# Страница фактов (персон или мест)
 @app.route('/facts')
 def facts():
-    return render_template('facts.html', title='Факты')
+    facts_cl = db['facts']
+    page = get_page_index()
 
+    facts = facts_cl.find({}).sort([('count', pymongo.DESCENDING)])
+    facts, pages = paginate(facts)
+    return render_template('facts.html', title='Факты', facts=facts, pages=pages, curpage=page)
+
+# Страница оценки тональности
 @app.route('/sentiment')
 def sentiment():
     return render_template('sentiment.html', title='Тональность')
